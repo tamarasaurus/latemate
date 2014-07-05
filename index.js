b58abce4c580ae2c9dba807bbd00ae07ca123e88
Tamara Chahine <chahine.tamara@abc.net.au>
2014-07-05 22:05:51 +1000

    Comment out index.js contents

---
+++ index.js
@@ -0,0 +1,13 @@
+// 'use strict';
+// var express = require('express');
+// var routes = require('./routes');
+
+// var app = express();
+
+// app.use(express.static(__dirname + '/public'));
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
+
+// routes(app);
+
+// app.listen(3000);
